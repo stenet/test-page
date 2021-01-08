3ae3858d2b48553080ee91d818d516b912f582b1
Stefan Heim <stefan.heim@hotmail.com>
2021-01-08 12:39:27 +0100

    Script angepasst

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("test").addEventListener("click", () => {
-    alert("you've clicked the button");
-  });
-}, {
-  once: true
+document.getElementById("test").addEventListener("click", () => {
+  alert("you've clicked the button");
 });
